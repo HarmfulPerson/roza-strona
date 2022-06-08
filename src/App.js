e4eec8eb5588121607a777142151384b09e1af22
Marcin Wieczorek <marcin.wieczorek@hawk-e.com.pl>
2022-06-08 14:03:18 +0200

    grupa ruszajaca sie

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Canvas, useFrame } from '@react-three/fiber';
-import React from 'react';
+/* eslint-disable react/prop-types */
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import React, { Suspense } from 'react';
 import {
   Extrude,
   OrbitControls,
@@ -7,51 +8,67 @@ import {
   Plane,
   Sphere,
   Stars,
+  useAnimations,
 } from '@react-three/drei';
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 import logo from './logo.svg';
 import './App.css';
 
-function MyRotatingBox() {
-  const myMesh = React.useRef();
-
-  useFrame(({ clock }) => {
-    const a = clock.getElapsedTime();
-    myMesh.current.rotation.x = a;
-  });
+const Object = ({ url, position, ...props }) => {
+  const { scene } = useLoader(GLTFLoader, url);
+  const { ref } = props;
+  const copiedScene = React.useMemo(() => scene.clone(), [scene]);
 
   return (
-    <mesh ref={myMesh}>
-      <boxBufferGeometry />
-      <meshPhongMaterial color="royalblue" />
-    </mesh>
+    <group>
+      <primitive scale={0.01} object={copiedScene} position={position} />
+    </group>
   );
-}
+};
+
+const Objects = () => {
+  const ref = React.useRef();
+  useFrame((state, delta, clock) => {
+    ref.current.position.z = state.clock.getElapsedTime();
+  });
+  return (
+    <group ref={ref}>
+      {[1, 2, 3, 4, 5, 6].map((lol) => (
+        <Object
+          key={lol}
+          position={[4 * lol, 1, 1]}
+          url="./drone_low_poly/scene.gltf"
+        />
+      ))}
+    </group>
+  );
+};
 
 function App() {
   const startingPosition = React.useRef([-1, 0, 0]);
-
+  const clockPositions = [0, 300, 600, 900];
   return (
     <div className="App">
       <div id="canvas-container">
         <Canvas
           dpr={window.devicePixelRatio}
-          camera={{ position: new THREE.Vector3(0, 0, 100) }}
+          camera={{ position: new THREE.Vector3(10, 25, 100) }}
           style={{ height: '100vh' }}
         >
           <color attach="background" args={['#06092c']} />
-          {/* <pointLight position={[-20, 10, 25]} /> */}
-          <Sphere position={[0, 10, 10]}>
-            <meshBasicMaterial color="hotpink" />
-          </Sphere>
+          <pointLight position={[-20, 10, 25]} />
+          <Suspense fallback={null}>
+            <Objects />
+          </Suspense>
           <gridHelper
             args={[100, 20, '#4D089A', '#4D089A']}
             position={[0, 0, 0]}
             rotation={[0, 0, 0]}
           />
           <Center />
-          {/* <OrbitControls /> */}
+          <OrbitControls />
           <Stars
             radius={100} // Radius of the inner sphere (default=100)
             depth={50} // Depth of area where stars should fit (default=50)
